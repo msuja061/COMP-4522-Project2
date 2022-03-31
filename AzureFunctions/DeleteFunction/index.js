6aff761cd9b1696fddb2e21de4a04af37de2bc6b
tmeil781 <47022215+tmeil781@users.noreply.github.com>
2022-03-31 09:10:58 -0600

    still dont know what im doing
    wrote some garbage

--- AzureFunctions/DeleteFunction/index.js
+++ AzureFunctions/DeleteFunction/index.js
@@ -1,13 +1,12 @@
+
+
+
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    const uri = "mongodb+srv://sqladminuser:@comp4522nosql.ljbwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        headers: { 'Content-Type': 'application/json' },
+        body: { res: 'Hello, World' }
     };
 }
