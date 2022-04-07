f7dd58200ecbb19ba4e63a09a558cb177bbd2e21
msuja061 <msuja061@mtroyal.ca>
2022-04-07 13:40:42 -0600

    Added post button and page

--- AzureFunctions/Comments-Function/index.js
+++ AzureFunctions/Comments-Function/index.js
@@ -1,4 +1,4 @@
-module.exports = async function (context, req) {
+/*module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
@@ -7,7 +7,152 @@ module.exports = async function (context, req) {
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
+        // status: 200, /* Defaults to 200 *
         body: responseMessage
     };
+
+    function UserAction() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+             if (this.readyState == 4 && this.status == 200) {
+                 alert(this.responseText);
+             }
+        };
+        xhttp.open("POST", "https://trighttp.azurewebsites.net/api/CommentQuery", true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        // xhttp.send("Your JSON Data Here");
+    }
+
+    /*using System.Net;
+    public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+    {
+    log.Info("C# HTTP trigger function processed a request.");
+    // parse query parameter
+    string name = req.GetQueryNameValuePairs().FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0).Value;
+    if (name == null)
+    {
+        // Get request body
+        dynamic data = await req.Content.ReadAsAsync<object>();
+        name = data?.name;
+    }
+    return name == null ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+        : req.CreateResponse(HttpStatusCode.OK, "Hello " + name);
+    }*/
+
+
+    /*// app.post() Method Demo Example
+    // Importing the express module
+    const express = require('express');
+
+    // Initializing the express and port number
+    var app = express();
+    // Initializing the router from express
+    var router = express.Router();
+    var PORT = 8080;
+
+    // Creating a POST request
+    app.post('https://trighttp.azurewebsites.net/api/CommentQuery', (req, res) => {
+    console.log("POST Request Called for /api endpoint")
+    res.send("POST Request Called")
+    })
+
+    // App listening on the below port
+    app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+    });*/
+
+    /*const express = require('express');
+    const app = express();
+    app.listen(3000, () => console.log('Listening on port 3000!'));
+    app.use(express.static('public'));
+    app.use(express.json());
+    app.post('/api/CommentQuery', (req, res) => {
+        console.log("POST Request Called for /api endpoint")
+        res.send("POST Request Called")
+    });*
+
+    // fetch('https://trighttp.azurewebsites.net/api/CommentQuery', {method: 'POST'})
+    // .then(result => result.json())
+    // .then(data => console.log(result));
+}*/
+
+const { ObjectID } = require('mongodb');
+const createClient = require('../shared/mongo'); // how to get shared file for mongo db on azure??
+ 
+module.exports = async function (context, req){
+   const { id } = req.params
+   const Comments_message = req.body || {}
+ 
+   if (!id || !Comments) {
+       context.res = {
+           status: 400,
+           body: 'Fields Required'
+       }
+       return;
+   }
+   const {db,connection} = await createClient()
+   const commentDB = db.collection('Comments')
+ 
+   switch(req.method){
+       case "PUT":
+       try {
+           const token = await commentDB.findOneAndUpdate(
+               { _id: ObjectId(id) },
+               { $set: Comments_message}
+               )
+               connection.close()
+               context.res = {
+                   status: 200,
+                   body: token
+               }
+           } catch (error) {
+               context.res = {
+                   status: 500,
+                   body: "Error Adding comment"
+               }
+           }
+           break;
+      
+       case "GET":
+           const res = await commentDB.find({}).limit(100) //grabs 100 comments
+           constbody = await res.toArray(); //puts 100 comments in array
+           connection.close()
+           context.res = { status: 200,
+           body //100 comments returned to the site
+       }
+       break;
+ 
+       case "POST":
+       try{
+           const c = await commentDB.insert(Comments_message)
+           connection.close()
+           context.res = {
+               status: 201,
+               body: c.ops[0]
+           }
+       }
+           catch (error) {
+           context.res = {
+               status: 500,
+               body: "Error Updating comment"
+           }
+       }
+       break;
+ 
+       case "DELETE":
+           try{
+               commentDB.findOneAndDelete({_id: ObjectId(id) })
+               connection.close()
+               context.res = {
+                   status: 204
+               }
+           }
+           catch (error) {
+               context.res = {
+                   status: 500,
+                   body: "Error Deleting comment"
+               }
+       }
+   }
 }
--- AzureFunctions/User-Function/index.js
+++ AzureFunctions/User-Function/index.js
@@ -10,4 +10,67 @@ module.exports = async function (context, req) {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+
+    /*// newly added; testing
+    const { Connection, Request } = require("tedious");
+ 
+    // Create connection to database
+    const config = {
+     authentication: {
+       options: {
+         userName: "sqladminuser",
+         password: "hwu48Vqp"
+       },
+       type: "default"
+     },
+     server: "comp4522-sqlserver.database.windows.net", // update me
+     options: {
+       database: "UserDB", //update me
+       encrypt: true
+     }
+    };
+     
+     
+    const connection = new Connection(config);
+     
+    // Attempt to connect and execute queries if connection goes through
+    connection.on("connect", err => {
+     if (err) {
+       console.error(err.message);
+     } else {
+       queryDatabase();
+     }
+    });
+     
+    connection.connect();
+     
+    function queryDatabase() {
+     console.log("Reading rows from the Table...");
+     
+     // Read all rows from table
+     const request = new Request(
+       `SELECT TOP (20) [id]
+       ,[userName]
+       ,[firstName]
+       ,[lastName]
+       ,[timeAdded]
+       ,[dateAdded]
+    FROM [dbo].[Users]`,
+       (err, rowCount) => {
+         if (err) {
+           console.error(err.message);
+         } else {
+           console.log(`${rowCount} row(s) returned`);
+         }
+       }
+     );
+     
+     request.on("row", columns => {
+       columns.forEach(column => {
+         console.log("%s\t%s", column.metadata.colName, column.value);
+       });
+     });
+     
+    //  connection.execSql(request);*/
+    
 }
